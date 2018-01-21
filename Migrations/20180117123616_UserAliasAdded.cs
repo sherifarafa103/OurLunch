d0b03b69c6823ea794cd0481af4e136c64440a4f
Sherif Arafa <sherifnarafa@aucegypt.edu>
2018-01-21 17:18:53 +0200

    Migrations

---
+++ Migrations/20180117123616_UserAliasAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace OurLunch.Migrations
+{
+    public partial class UserAliasAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Alias",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Alias",
+                table: "User");
+        }
+    }
+}

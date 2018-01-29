fa38590e209bb3fe53d53f3462b8c591a07975b9
Omar Sourour <omsour@microsoft.com>
2018-01-30 00:29:58 +0200

    Finished sign in component.

--- Client/src/pages/sign-in/sign-in.component.ts
+++ Client/src/pages/sign-in/sign-in.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController, LoadingController } from 'ionic-angular';
 import { UserService } from '../../services/user.service';
+import { BaseService } from '../../services/base.service';
+import { MainPage } from '../main/main.component';
 
 @Component({
     selector: 'sign-in',
@@ -9,16 +11,28 @@ import { UserService } from '../../services/user.service';
 export class SignInPage {
     public alias: string = "";
 
-    constructor(public navCtrl: NavController, private _userService: UserService) {
+    constructor(
+        private _viewController: ViewController,
+        private _navController: NavController,
+        private _baseService: BaseService,
+        private _loadingController: LoadingController,
+        private _userService: UserService
+    ) { }
 
+    public dismiss(): void {
+        this._viewController.dismiss();
     }
 
-    signIn() {
+    public signIn(): void {
         if (this.alias.trim() !== "") {
-            this._userService.signIn(this.alias).subscribe((user) => {
+            let loader = this._loadingController.create({ content: "Signing in" });
+            loader.present();
 
-            }, (error) => alert("Error"));
+            this._userService.signIn(this.alias)
+                .finally(() => loader.dismiss())
+                .subscribe(() => this._navController.push(MainPage), () => this._baseService.showErrorToast(
+                    "User does not exist or a server error occurred.")
+                );
         }
     }
-
 }

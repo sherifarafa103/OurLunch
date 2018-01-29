b8bbeb14787e54707da7f81a0df0c47969774bc5
Omar Sourour <omsour@microsoft.com>
2018-01-30 00:31:08 +0200

    Save current user in user service.

--- Client/src/services/user.service.ts
+++ Client/src/services/user.service.ts
@@ -3,9 +3,12 @@ import { Http } from '@angular/http';
 import { User } from '../models/user.model';
 import { BaseService } from '../services/base.service';
 import { Observable } from 'rxjs/Rx';
+import 'rxjs';
 
 @Injectable()
 export class UserService {
+    public currentUser: User;
+
     constructor(
         private _baseService: BaseService,
         private _httpService: Http
@@ -13,11 +16,14 @@ export class UserService {
 
     public signIn(alias: string): Observable<User> {
         return this._httpService.get(`${this._baseService.baseUrl}/users/alias/${alias}`)
-            .map(r => r.json());
+            .map(r => r.json())
+            .do(user => this.currentUser = user);
     }
 
     public add(user: User): Observable<number> {
         return this._httpService.post(`${this._baseService.baseUrl}/users`, user)
-            .map(r => r.json());
+            .map(r => r.json())
+            .do(id => user.id = id)
+            .do(() => this.currentUser = user);
     }
 }

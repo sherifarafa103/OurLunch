f9c794c6eaa0a79312e52ecdd58d38ce7ecbba19
Omar Sourour <omsour@microsoft.com>
2018-01-30 00:30:55 +0200

    Added toaster handling to base service.

--- Client/src/services/base.service.ts
+++ Client/src/services/base.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 
 @Injectable()
 export class BaseService {
+    constructor(private _toastController: ToastController) { }
 
     public get baseUrl(): string {
-        return 'http://localhost:55013/api';
+        return 'http://localhost:55014/api';
+    }
+
+    public showErrorToast(message: string): void {
+        this._toastController
+            .create({ message: message, duration: 5000, showCloseButton: true })
+            .present();
     }
 }

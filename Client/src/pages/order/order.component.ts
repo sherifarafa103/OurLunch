abd47d3c3ea8aea8f53e96e671890e43276373ce
Omar Sourour <omsour@microsoft.com>
2018-01-30 17:41:33 +0200

    Added order page component data.

--- Client/src/pages/order/order.component.ts
+++ Client/src/pages/order/order.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Order } from '../../models/order.model';
+import { UserService } from '../../services/user.service';
+import { RestaurantService } from '../../services/restaurant.service';
+import { MealService } from '../../services/meal.service';
+import { Observable } from 'rxjs/Rx';
+import { Restaurant } from '../../models/restaurant.model';
+import { User } from '../../models/user.model';
+import { Meal } from '../../models/meal.model';
 
 @Component({
     selector: 'order-page',
     templateUrl: 'order.component.html'
 })
 export class OrderPage {
+    public order: Order;
+    public restaurant: Observable<Restaurant>;
+    public users: Observable<User[]>;
+    public meals: Observable<Meal[]>;
+    public user: Observable<User>;
+    public time: Observable<number>;
 
-    constructor(public navCtrl: NavController) { }
+    constructor(
+        private _navParams: NavParams,
+        private _restaurantService: RestaurantService,
+        private _mealService: MealService,
+        private _userService: UserService
+    ) { }
+
+    public ngOnInit(): void {
+        this._initState();
+    }
+
+    private _initState(): void {
+        this.order = this._navParams.get('order');
+        this.restaurant = this._restaurantService.get()
+            .map(restaurants => restaurants.find(r => r.restaurantId === this.order.restaurantId));
+
+        this.users = this._userService.get();
+
+        this.meals = this._mealService.get(this.order.restaurantId);
+
+        this.user = this.users.map(users => users.find(u => u.userId === this.order.userId));
+
+        this.time = Observable.timer(0, 60000)
+            .map(() => this._getMinuteDifference(this.order.time, new Date()));
+    }
+
+    private _getMinuteDifference(date2: Date, date1: Date) {
+        var diff = (date2.getTime() - date1.getTime()) / 1000;
+        diff /= 60;
+
+        return Math.abs(Math.round(diff));
+    }
 }
--- Client/src/services/user.service.ts
+++ Client/src/services/user.service.ts
@@ -14,6 +14,11 @@ export class UserService {
         private _httpService: Http
     ) { }
 
+    public get(id?: number): Observable<User[]> {
+        return this._httpService.get(`${this._baseService.baseUrl}/users/${id}`)
+            .map(r => r.json());
+    }
+
     public signIn(alias: string): Observable<User> {
         return this._httpService.get(`${this._baseService.baseUrl}/users/alias/${alias}`)
             .map(r => r.json())
@@ -23,7 +28,7 @@ export class UserService {
     public add(user: User): Observable<number> {
         return this._httpService.post(`${this._baseService.baseUrl}/users`, user)
             .map(r => r.json())
-            .do(id => user.id = id)
+            .do(id => user.userId = id)
             .do(() => this.currentUser = user);
     }
 }

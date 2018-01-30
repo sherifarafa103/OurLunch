87a5d963a215e7d666b0c05510aebcb27d397c5e
Omar Sourour <omsour@microsoft.com>
2018-01-30 18:36:04 +0200

    Order item implements IResource

--- Client/src/models/orderItem.model.ts
+++ Client/src/models/orderItem.model.ts
@@ -1,14 +1,31 @@
-export class OrderItem {
+import { IResource } from "../interfaces/IResource";
+
+export class OrderItem implements IResource {
+    id: number;
     orderId: number;
     userId: number;
     mealId: number;
     price: number;
 
 
-    constructor(orderId, userId, mealId, price: number = 0) {
+    constructor(id: number, orderId: number, userId: number, mealId: number, price: number = 0) {
         this.orderId = orderId;
         this.userId = userId;
         this.mealId = mealId;
         this.price = price;
+    }
+
+    static importFromApi(apiObject): OrderItem {
+        return new OrderItem(apiObject.orderItemId, apiObject.orderId, apiObject.userId, apiObject.mealId, apiObject.price);
+    }
+
+    public exportToApi(): Object {
+        return {
+            orderItemId: this.id,
+            orderId: this.id,
+            userId: this.userId,
+            mealId: this.mealId,
+            price: this.price
+        };
     }
 }

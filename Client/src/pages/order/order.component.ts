71954f7f3858fb3fcb9ad659237596d79f6c0411
Omar Sourour <omsour@microsoft.com>
2018-02-06 01:39:43 +0200

    Adjustments to order service and component.

--- Client/src/pages/order/order.component.ts
+++ Client/src/pages/order/order.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { DatePipe } from '@angular/common';
+import { NavParams } from 'ionic-angular';
 import { Order } from '../../models/order.model';
 import { UserService } from '../../services/user.service';
 import { RestaurantService } from '../../services/restaurant.service';
@@ -8,43 +9,133 @@ import { Observable } from 'rxjs/Rx';
 import { Restaurant } from '../../models/restaurant.model';
 import { User } from '../../models/user.model';
 import { Meal } from '../../models/meal.model';
+import { OrderItem } from '../../models/orderItem.model';
+import { OrderItemService } from '../../services/orderItem.service';
+import { ModalController } from 'ionic-angular/components/modal/modal-controller';
+import { Modal } from 'ionic-angular/components/modal/modal';
+import { ItemPopover } from './item-popover/item-popover.component';
+import { OrderService } from '../../services/order.service';
+import { SettingsPopover } from './settings-popover/settings-popover.component';
+
+export interface IUserOrderItems {
+    userId: number;
+
+    orderItems: OrderItem[];
+}
 
 @Component({
     selector: 'order-page',
-    templateUrl: 'order.component.html'
+    templateUrl: 'order.component.html',
+    providers: [DatePipe]
 })
 export class OrderPage {
     public order: Order;
+    public tax: number = 0;
+    public delivery: number = 0;
+
     public restaurant: Observable<Restaurant>;
     public users: Observable<User[]>;
     public meals: Observable<Meal[]>;
     public user: Observable<User>;
-    public time: Observable<number>;
+    public time: Observable<string>;
+    public userItems: Observable<IUserOrderItems[]>;
 
     constructor(
         private _navParams: NavParams,
         private _restaurantService: RestaurantService,
         private _mealService: MealService,
-        private _userService: UserService
+        private _userService: UserService,
+        private _orderItemService: OrderItemService,
+        private _modalController: ModalController,
+        private _orderService: OrderService,
+        private _datePipe: DatePipe
     ) { }
 
     public ngOnInit(): void {
         this._initState();
     }
 
+    public updateOrder(): void {
+        this.order.tax = this.tax;
+        this.order.delivery = this.delivery;
+
+        this._orderService.update(this.order.id, this.order)
+            .subscribe();
+    }
+
+    public openSettingsModal(event: Event): void {
+        const orderClone: Order = new Order(
+            this.order.id,
+            this.order.userId,
+            this.order.restaurantId,
+            this.order.time,
+            this.order.tax,
+            this.order.delivery
+        );
+        const modal: Modal = this._modalController.create(SettingsPopover, { order: orderClone });
+        modal.present({ ev: event });
+
+        modal.onDidDismiss(order => {
+            if (order) {
+                this._orderService.update(order.id, order)
+                    .subscribe(() => this.order = order);
+            }
+        });
+    }
+
+    public openItemModal(event: Event): void {
+        const modal: Modal = this._modalController.create(ItemPopover, { restaurantId: this.order.restaurantId, orderId: this.order.id });
+        modal.present({ ev: event });
+
+        modal.onDidDismiss(item => {
+            if (item) {
+                this._orderItemService.add(item)
+                    .subscribe();
+            }
+        });
+    }
+
+    public calculateTotal(userItem: IUserOrderItems): number {
+        const itemsPrices: number = userItem.orderItems.reduce((acc, val) => acc + val.price * val.quantity, 0);
+        return itemsPrices + (itemsPrices * this.tax) / 100;
+    }
+
     private _initState(): void {
         this.order = this._navParams.get('order');
+
         this.restaurant = this._restaurantService.get()
             .map(restaurants => restaurants.find(r => r.id === this.order.restaurantId));
 
         this.users = this._userService.get();
 
-        this.meals = this._mealService.get(this.order.restaurantId);
-
         this.user = this.users.map(users => users.find(u => u.id === this.order.userId));
 
+        this.meals = this._mealService.get(this.order.restaurantId);
+
+        this.userItems = this._orderItemService.get(this.order.id).map(orderItems => {
+            const userMap: Map<number, OrderItem[]> = new Map<number, OrderItem[]>();
+
+            orderItems.forEach(item => {
+                if (userMap.has(item.userId)) {
+                    userMap.get(item.userId).push(item);
+                }
+                else {
+                    userMap.set(item.userId, [item]);
+                }
+            });
+
+            return Array.from(userMap.entries()).map(entry => ({ userId: entry[0], orderItems: entry[1] }));
+        });
+
         this.time = Observable.timer(0, 60000)
-            .map(() => this._getMinuteDifference(this.order.time, new Date()));
+            .map(() => {
+                const now: Date = new Date();
+                const orderTime: Date = this.order.time;
+
+                return orderTime > now ?
+                    `${this._getMinuteDifference(orderTime, now)} mins`
+                    : `${this._datePipe.transform(orderTime, 'short')}`;
+            });
     }
 
     private _getMinuteDifference(date2: Date, date1: Date) {
--- Client/src/services/order.service.ts
+++ Client/src/services/order.service.ts
@@ -36,10 +36,29 @@ export class OrderService {
     }
 
     public update(id: number, order: Order): Observable<void> {
-        return this._cacheService.put(`${this._baseService.baseUrl}/orders/${id}`, order);
+        return this._cacheService.put(`${this._baseService.baseUrl}/orders`, order);
     }
 
     public delete(id: number): Observable<void> {
         return this._cacheService.delete(`${this._baseService.baseUrl}/orders`, id);
+    }
+
+    private _toLocalTimezone(date: Date): string {
+        let pad = n => n < 10 ? '0' + n : n;
+        let tz = date.getTimezoneOffset();
+        let tzs = `${(tz > 0 ? "-" : "+")}${pad(parseInt(<any>Math.abs(tz / 60)))}`;
+
+        if (tz % 60 != 0)
+            tzs += pad(Math.abs(tz % 60));
+
+        if (tz === 0) {
+            tzs = 'Z';
+        }
+
+        return date.getFullYear() + '-'
+            + pad(date.getMonth() + 1) + '-'
+            + pad(date.getDate()) + 'T'
+            + pad(date.getHours()) + ':'
+            + pad(date.getMinutes());
     }
 }

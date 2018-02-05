96f0643b84814927ad96b7f1171ead82e590bcbe
Omar Sourour <omsour@microsoft.com>
2018-02-05 11:42:06 +0200

    Added active orders component.

--- Client/src/pages/active-orders/active-orders.component.ts
+++ Client/src/pages/active-orders/active-orders.component.ts
@@ -1,11 +1,56 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Rx';
+import { Order } from '../../models/order.model';
+import { Restaurant } from '../../models/restaurant.model';
+import { User } from '../../models/user.model';
+import { OrderService } from '../../services/order.service';
+import { RestaurantService } from '../../services/restaurant.service';
+import { UserService } from '../../services/user.service';
+import { OrderPage } from '../order/order.component';
 
 @Component({
     selector: 'active-orders-page',
-    templateUrl: 'active-orders.component.html'
+    templateUrl: 'active-orders.component.html',
+    providers: [DatePipe]
 })
 export class ActiveOrdersPage {
+    public orders: Observable<Order[]>;
+    public users: Observable<User[]>;
+    public restaurants: Observable<Restaurant[]>;
 
-    constructor(public navCtrl: NavController) { }
+    constructor(
+        private _orderService: OrderService,
+        private _userService: UserService,
+        private _restaurantService: RestaurantService,
+        private _navController: NavController,
+        private _loadingController: LoadingController
+    ) { }
+
+    public ngOnInit(): void {
+        this._initState();
+    }
+
+    public search(): void {
+        this._getNewOrders();
+    }
+
+    public goToOrder(order: Order): void {
+        this._navController.push(OrderPage, { order: order });
+    }
+
+    private _initState(): void {
+        this._getNewOrders();
+        this.users = this._userService.get();
+        this.restaurants = this._restaurantService.get();
+    }
+
+    private _getNewOrders(): void {
+        const loader = this._loadingController.create({ content: "Loading Orders" });
+        this.orders = this._orderService.getActiveOrders()
+            .map(orders => orders.sort((a, b) => a.time > b.time ? 1 : -1))
+            .finally(() => loader.dismiss())
+            .share();
+    }
 }
